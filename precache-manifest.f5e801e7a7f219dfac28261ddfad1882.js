21e6840cbb71ac4c5dd72327aaa073bafc44c46f
Gabriel Nascimento <gabrielgrames@gmailcom>
2020-02-21 14:25:22 -0400

    Updates

---
+++ precache-manifest.f5e801e7a7f219dfac28261ddfad1882.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac238c17cd57bd228ea29964ff67d60",
+    "url": "/Bin2Dec/index.html"
+  },
+  {
+    "revision": "0a7ad58330074c8a6036",
+    "url": "/Bin2Dec/static/css/main.b130d65f.chunk.css"
+  },
+  {
+    "revision": "0fe54f7f4d05d6fa755e",
+    "url": "/Bin2Dec/static/js/2.a126944b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Bin2Dec/static/js/2.a126944b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a7ad58330074c8a6036",
+    "url": "/Bin2Dec/static/js/main.e2664f4f.chunk.js"
+  },
+  {
+    "revision": "7d78a89df9bd3b7caa3e",
+    "url": "/Bin2Dec/static/js/runtime-main.454f4e0f.js"
+  }
+]);
